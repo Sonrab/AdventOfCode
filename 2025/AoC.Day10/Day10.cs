1da2e5816316b44c136b7db016e51b5e63e4db52
Barret <mysbarret@gmail.com>
2025-12-31 14:31:50 +0100

    Fix

--- 2025/AoC.Day10/Day10.cs
+++ 2025/AoC.Day10/Day10.cs
@@ -16,8 +16,7 @@ static long Part1()
         for(int i = 1; i <= wiringSchematics.Length; i++)
         {
             int[] arr = [.. Enumerable.Range(0, wiringSchematics.Length)];
-            int r = i;
-            var schematicIndexCombinations = Helper.FindCombinations(arr, r);
+            var schematicIndexCombinations = Helper.FindCombinations(arr, i);
 
             foreach (var schematicIndexes in schematicIndexCombinations) 
             {
@@ -44,39 +43,7 @@ static long Part2()
 {
     var rows = "AoC.Day10/input.txt".ReadAllLines();
 
-    return rows.Sum(row =>
-    {
-        var indicatorLightDiagram = Regex.Match(row, @"\[(.*)\]").Groups[1].Value
-            .Select(v => v == '.' ? 0 : 1).ToArray();
-        var joltageRequirements = Regex.Match(row, @"\{(.*)\}").Groups[1].Value;
-        var wiringSchematics = Regex.Matches(row, @"(?<=\()[^)]*(?=\))")
-            .Select(m => m.Value.Split(",").Select(s => int.Parse(s))).ToArray();
-
-        for(int i = 1; i <= wiringSchematics.Length; i++)
-        {
-            int[] arr = [.. Enumerable.Range(0, wiringSchematics.Length)];
-            int r = i;
-            var schematicIndexCombinations = Helper.FindCombinations(arr, r);
-
-            foreach (var schematicIndexes in schematicIndexCombinations) 
-            {
-                var indicatorLights = new int[indicatorLightDiagram.Length];
-                var flips = schematicIndexes.SelectMany(s => wiringSchematics[s]);
-
-                foreach (var flip in flips) 
-                {
-                    indicatorLights[flip] = indicatorLights[flip] == 0 ? 1 : 0;
-                }
-
-                if(indicatorLightDiagram.SequenceEqual(indicatorLights))
-                {
-                    return i;
-                }
-            }
-        }
-
-        throw new Exception();
-    });
+    return 0;
 }
 
 Console.WriteLine(Part1());
